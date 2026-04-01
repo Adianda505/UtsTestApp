a82a345807fd0be0edb466cb4600de561f3e2b13
Adianda505 <adiazhar38@gmail.com>
2026-04-01 23:15:38 +0700

    Add layout admin

---
+++ resources/views/layout/admin.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" class="dark">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </title>
+    <title>Admin Panel</title>
+</head>
+<body> 
+<div class="bg-white dark:bg-white">
+        
+<div class="flex flex-col bg-gray-100 ml-45">
+    @include('components.sidebarMenu')
+</div>
+
+</div>
+</body>
+</html>
