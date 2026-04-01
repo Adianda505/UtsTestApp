86abf4844edb3fb6fe09d91b67d998456351fe44
Adianda505 <adiazhar38@gmail.com>
2026-04-01 23:19:53 +0700

    add other routes page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/pages/dashboard');
 });
+
+Route::view('/pages/dashboard',  'pages.dashboard')->name('admin.dashboard');
+Route::view('/pages/products',   'pages.products')->name('admin.products');
+Route::view('/pages/reports',    'pages.customers')->name('admin.customers');
+Route::view('/pages/statistics', 'pages.transaction')->name('admin.transaction');
+Route::view('/pages/settings',  'pages.settings')->name('admin.settings');
