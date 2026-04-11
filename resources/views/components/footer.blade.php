51a65f905786a496e3be0734c5024ea122176322
unknown <editiaatawaqal@gmail.com>
2026-04-11 22:29:41 +0700

    namefooter

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-7" alt="Flowbite Logo" />
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-7" alt="UsMart" />
                 <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-body sm:mb-0">
@@ -23,7 +23,6 @@
             </ul>
         </div>
         <hr class="my-6 border-default sm:mx-auto lg:my-8" />
-        <span class="block text-sm text-body sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-body sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">UsMart™</a>. All Rights Reserved.</span>
     </div>
 </footer>
-
